3826833f7e6dc23f0463756625d914f44bc8d861
Karl Hiner <karl.hiner@gmail.com>
2017-12-20 11:22:52 -0800

    Append trailing slash to all links to make react-snapshot generate seperate folders with index.html files for each link

--- src/MainNav.js
+++ src/MainNav.js
@@ -16,32 +16,32 @@ class MainNav extends Component {
         <Navbar.Collapse>
           <Nav>
             <NavDropdown title="Processing" id="processing">
-              <LinkContainer to="/processing/retrograde_motion">
+              <LinkContainer to="/processing/retrograde_motion/">
                 <MenuItem>Retrograde motion</MenuItem>
               </LinkContainer>
-              <LinkContainer to="/processing/string_pluck">
+              <LinkContainer to="/processing/string_pluck/">
                 <MenuItem>String pluck</MenuItem>
               </LinkContainer>
-              <LinkContainer to="/processing/snow_globe">
+              <LinkContainer to="/processing/snow_globe/">
                 <MenuItem>Snow globe</MenuItem>
               </LinkContainer>
-              <LinkContainer to="/processing/force_graph">
+              <LinkContainer to="/processing/force_graph/">
                 <MenuItem>Force-directed graphs</MenuItem>
               </LinkContainer>
-              <LinkContainer to="/processing/bubble_wrap">
+              <LinkContainer to="/processing/bubble_wrap/">
                 <MenuItem>Bubble Wrap</MenuItem>
               </LinkContainer>
             </NavDropdown>
             <NavDropdown title="Music generation" id="music_generation">
-              <LinkContainer to="/music_generation/midi_markov">
+              <LinkContainer to="/music_generation/midi_markov/">
                 <MenuItem>MIDI Markov</MenuItem>
               </LinkContainer>
-              <LinkContainer to="/music_generation/auto_sampler">
+              <LinkContainer to="/music_generation/auto_sampler/">
                 <MenuItem>AutoSampler</MenuItem>
               </LinkContainer>
             </NavDropdown>
             <NavDropdown title="Audio DSP" id="audio_dsp">
-              <LinkContainer to="/audio_dsp/jupyter_notebooks">
+              <LinkContainer to="/audio_dsp/jupyter_notebooks/">
                 <MenuItem>Jupyter notebooks</MenuItem>
               </LinkContainer>
             </NavDropdown>
