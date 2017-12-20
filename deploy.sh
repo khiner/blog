04fba3a12ffc84ab8163e365a9b89ea4b4c2848b
Karl Hiner <karl.hiner@gmail.com>
2017-12-20 15:38:35 -0800

    eh, just remove everything in public html before deploying. Don't want stale foldersd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tar -czf build.tar.gz build
+ssh -p 21098 karlerbd@server144.web-hosting.com "cd public_html && rm -rf *"
 scp -P 21098 build.tar.gz karlerbd@server144.web-hosting.com:public_html/
 ssh -p 21098 karlerbd@server144.web-hosting.com "cd public_html && tar -xvzf build.tar.gz && rsync -a build/ ./ && rm -r build/ build.tar.gz"
 rm build.tar.gz
