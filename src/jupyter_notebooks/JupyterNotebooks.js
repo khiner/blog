6794dffb06bad418ae69252cc973d168878d735a
Karl Hiner <karl.hiner@gmail.com>
2018-01-02 12:54:21 -0700

    padding above share links in all cases

--- src/jupyter_notebooks/JupyterNotebooks.js
+++ src/jupyter_notebooks/JupyterNotebooks.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default (
   <div>
-    <a
-      href="https://nbviewer.jupyter.org/github/khiner/notebooks/blob/master/test_audio.ipynb"
-      target="_blank"
-      rel="noopener noreferrer">
-      Test Audio
-    </a>{' '}
-    is an example of outputting audio using Numpy arrays.
+    <p>
+      <a
+        href="https://nbviewer.jupyter.org/github/khiner/notebooks/blob/master/test_audio.ipynb"
+        target="_blank"
+        rel="noopener noreferrer">
+        Test Audio
+      </a>{' '}
+      is an example of outputting audio using Numpy arrays.
+    </p>
   </div>
 )
