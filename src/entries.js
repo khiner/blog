63a3b62978e17ce6ca4428cbc9b25f659af5f1e2
Karl Hiner <karl.hiner@gmail.com>
2018-01-28 11:26:49 -0800

    small typo fix - don't escape single quote in description

--- src/entries.js
+++ src/entries.js
@@ -153,7 +153,7 @@ const entries = [
       <div>
         <p>
           A collection of tracks I published spanning from 2012 to 2017. There
-          isn\'t any cohesive theme. Special guests Andrew Knight (on Riders of
+          isn't any cohesive theme. Special guests Andrew Knight (on Riders of
           the Night - the Raccoon Song) and pLiskin (on Elise and Paw).
         </p>
         <AlbumPreview title="Web Tracks 2012 - 2017" playlistId="5017148" />
