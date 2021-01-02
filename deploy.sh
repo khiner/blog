415b6b768b63bfa5dfb6873d994e99ee3bd00d52
Karl Hiner <karl.hiner@gmail.com>
2021-01-01 16:59:19 -0800

    Update deploy script to point to new hosting VPS

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npm run build
 tar -czf build.tar.gz build
-ssh -p 21098 karlerbd@server144.web-hosting.com "cd public_html && rm -rf *"
-scp -P 21098 build.tar.gz karlerbd@server144.web-hosting.com:public_html/
-ssh -p 21098 karlerbd@server144.web-hosting.com "cd public_html && tar -xvzf build.tar.gz && rsync -a build/ ./ && rm -r build/ build.tar.gz"
+ssh -p 7822 root@64.4.160.10 "cd /var/www/html && rm -rf *"
+scp -P 7822 build.tar.gz root@64.4.160.10:/var/www/html/
+ssh -p 7822 root@64.4.160.10 "cd /var/www/html && tar -xvzf build.tar.gz && rsync -a build/ ./ && rm -r build/ build.tar.gz"
 rm build.tar.gz
