92eef68a95ab234c7007b403b7aba8de01b2df4f
Karl Hiner <karl.hiner@gmail.com>
2021-01-07 16:13:06 -0800

    Replace IP address with hostname now that nameserver stuff is worked out

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npm run build
 tar -czf build.tar.gz build
-ssh -p 7822 root@64.4.160.10 "cd /var/www/html && rm -rf *"
-scp -P 7822 build.tar.gz root@64.4.160.10:/var/www/html/
-ssh -p 7822 root@64.4.160.10 "cd /var/www/html && tar -xvzf build.tar.gz && rsync -a build/ ./ && rm -r build/ build.tar.gz"
+ssh -p 7822 root@karlhiner.com "cd /var/www/html && rm -rf *"
+scp -P 7822 build.tar.gz root@karlhiner.com:/var/www/html/
+ssh -p 7822 root@karlhiner.com "cd /var/www/html && tar -xvzf build.tar.gz && rsync -a build/ ./ && rm -r build/ build.tar.gz"
 rm build.tar.gz
