4f86a9891aebb3b8c22fb4b10b44f10efd3fa4f2
Karl Hiner <karl.hiner@gmail.com>
2024-05-25 10:29:49 -0400

    Fix layout in drum classification post

--- src/content/drum_classification/DrumClassification.tsx
+++ src/content/drum_classification/DrumClassification.tsx
@@ -71,7 +71,7 @@ export default (
         display: 'flex',
         flexWrap: 'wrap',
         gap: '10px',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
         marginBottom: '1em',
       }}
